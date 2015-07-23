185878011aa52e5db5733def9f53d496912fb722
Nicolae Grigoriu <ng@inleadmedia.com>
2015-07-23 16:38:04 +0300

    DDBEASY-706 - Add clickable option for MARC field.

--- ting_marc.js
+++ ting_marc.js
@@ -6,9 +6,8 @@
     processing = 'ting-marc-processing',
     nodata = 'ting-marc-nodata';
 
-  function ting_marc_fields(container) {
+  function ting_marc_fields(container, settings) {
     var fields = [];
-
     $('.' + selector, container).each(function() {
       var data = $(this).data('ting-marc');
       if (data) {
@@ -17,7 +16,6 @@
       $(this).removeClass(selector);
       $(this).addClass(processing);
     });
-
     if (fields.length > 0) {
       $.post(
         Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting/marc/fields',
@@ -26,12 +24,21 @@
           $('.' + processing, container).each(function(){
             var field = $(this),
               _data = field.data('ting-marc');
-
             if (typeof(data[_data]) == "undefined") {
               field.addClass(nodata);
             }
             else {
-              field.find('.field-item').html(data[_data]);
+              if (Drupal.settings.clickable == 1) {
+                var links = data[_data].split(", ");
+                var linkdata = [];
+                for (var i = 0; i < links.length; i++) {
+                  linkdata.push('<a href="/search/ting/' + Drupal.settings.link_index + '=' + links[i] + '">' + links[i] + '</a>');
+                };
+                field.find('.field-item').html(linkdata.join(', '));
+              }
+              else {
+                field.find('.field-item').html(data[_data]);
+              };
             }
             field.removeClass(processing);
           });
@@ -41,8 +48,8 @@
   }
 
   Drupal.behaviors.ting_marc = {
-    attach : function(context) {
-      ting_marc_fields(context);
+    attach : function(context, settings) {
+      ting_marc_fields(context, settings);
     }
   };
 
