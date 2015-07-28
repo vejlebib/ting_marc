b0db53cb51f2bf8c500dad2f1eaa1f51be8e2fcf
Nicolae Grigoriu <ng@inleadmedia.com>
2015-07-28 14:58:41 +0300

    DDBEASY-706 - Update coding standards.

--- ting_marc.js
+++ ting_marc.js
@@ -19,9 +19,10 @@
     if (fields.length > 0) {
       $.post(
         Drupal.settings.basePath + Drupal.settings.pathPrefix + 'ting/marc/fields',
-        {ting_marc_fields: fields,
-         ting_marc_clickable: Drupal.settings.clickable,
-         ting_marc_link_index: Drupal.settings.link_index
+        {
+          ting_marc_fields: fields,
+          ting_marc_clickable: Drupal.settings.clickable,
+          ting_marc_link_index: Drupal.settings.link_index
         },
         function(data) {
           $('.' + processing, container).each(function(){
